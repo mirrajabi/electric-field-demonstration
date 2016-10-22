0769375c0eedde0da40c4f6a249b67855cb6ea2a
Mohammad Mirrajabi <mohammadmirrajabi@gmail.com>
2016-10-23 00:18:24 +0330

    Initial Commit

---
+++ Scripts/QParticle.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QParticle : MonoBehaviour
+{
+    public long ElectricCharge;
+	
+	void Start()
+    {
+        ElectricCharge = 1;
+	}
+
+    public void SetValue(long value)
+    {
+        ElectricCharge = value;
+        if(value >= 0)
+        {
+            GetComponent<Renderer>().material = EntityManager.Instance.ChargePositive;
+        }
+        else
+        {
+            GetComponent<Renderer>().material = EntityManager.Instance.ChargeNegative;
+        }
+    }
+
+    public void InvertCharge()
+    {
+        SetValue(-ElectricCharge);
+    }
+}
